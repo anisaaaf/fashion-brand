cf7e63d6d205d733a0335e4358d67b28a0832259
Anisa <af69153@ubt-uni.net>
2023-12-07 10:39:15 -1100

    anisa's work

---
+++ fashionbrand.js
@@ -0,0 +1,33 @@
+const loginBtn = document.querySelector(".btnLogin-popup");
+const closeBtn = document.querySelector(".icon-close");
+const loginCtn = document.querySelector(".wrapper");
+const registerLink = document.querySelector(".register-link");
+const wrapper = document.querySelector(".wrapper");
+const loginLink = document.querySelector(".login-link")
+
+wrapper.style.display = "none";
+
+function showHideElement() {
+    if (loginCtn.style.display === "none") {
+        loginCtn.style.display = "block";
+    } else {
+        loginCtn.style.display = "none";
+    }
+  }
+
+closeBtn.addEventListener("click", ()=>{
+    loginCtn.style.display = "none";
+    wrapper.classList.remove("active");
+})
+
+loginBtn.addEventListener("click", ()=>{
+    showHideElement();
+})
+
+registerLink.addEventListener("click",()=>{
+    wrapper.classList.add("active");
+})
+
+loginLink.addEventListener("click",()=>{
+    wrapper.classList.remove("active");
+})
