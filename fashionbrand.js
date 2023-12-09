8044a806b207595fd3987dbcf88e361e941f069c
Anisa <af69153@ubt-uni.net>
2023-12-09 00:46:13 -1100

    anisa

--- fashionbrand.js
+++ fashionbrand.js
@@ -1,7 +1,7 @@
 const loginBtn = document.querySelector(".btnLogin-popup");
 const closeBtn = document.querySelector(".icon-close");
 const loginCtn = document.querySelector(".wrapper");
-const registerLink = document.querySelector(".register-link"); /* aktivizimi i butonit register*/
+const registerLink = document.querySelector(".register-link");
 const wrapper = document.querySelector(".wrapper");
 const loginLink = document.querySelector(".login-link")
 
@@ -31,3 +31,134 @@ registerLink.addEventListener("click",()=>{
 loginLink.addEventListener("click",()=>{
     wrapper.classList.remove("active");
 })
+
+let errorCount = 0
+function validateForm() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    let emailError = document.getElementById("email-error")
+    let passwordError = document.getElementById("password-error")
+
+    
+    if (email.trim() === "") {
+        emailError.innerHTML = "Please enter your email."
+        emailError.style.display = "block"
+        errorCount ++
+        return;
+    } else {
+        emailError.innerHTML = ""
+        emailError.style.display = "none"
+        errorCount--
+    }
+
+    var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/;
+
+    if (!emailPattern.test(email)) {
+        emailError.innerHTML = "Invalid email format. Please enter a valid email address."
+        emailError.style.display = "block"
+        // alert("Invalid email format. Please enter a valid email address.");
+        return;
+    }  else {
+        emailError.innerHTML = ""
+        emailError.style.display = "none"
+        errorCount--
+    }
+
+
+    
+    if (password.trim() === "") {
+        passwordError.innerHTML = "Please enter your password."
+        passwordError.style.display = "block"
+        errorCount ++
+        // alert("Please enter your password.");
+        return;
+    }  else {
+        passwordError.innerHTML = ""
+        passwordError.style.display = "none"
+        errorCount--
+    }
+
+    if (password.length < 8) {
+        passwordError.innerHTML = "Password must be at least 8 characters long."
+        passwordError.style.display = "block"
+        errorCount ++
+        // alert("Password must be at least 8 characters long.");
+        return;
+    }   else {
+        passwordError.innerHTML = ""
+        passwordError.style.display = "none"
+        errorCount--
+    }
+
+    if(errorCount > 0){
+        alert("Form is invalid! fix the error.");
+    }
+    alert("Form is valid! You can submit it.");
+}
+
+let resigterErrorCount = 0
+function validateRegisterForm() {
+    var username = document.getElementById('register-username').value;
+    var email = document.getElementById('register-email').value;
+    var password = document.getElementById('register-password').value;
+    let usernameError = document.getElementById("username-register-error")
+    let emailError = document.getElementById("email-register-error")
+    let passwordError = document.getElementById("password-register-error")
+
+    
+    if (username.trim() === "") {
+        usernameError.innerHTML = "Please enter your username."
+        usernameError.style.display = "block"
+        resigterErrorCount ++
+        return;
+    } else {
+        usernameError.innerHTML = ""
+        usernameError.style.display = "none"
+        resigterErrorCount--
+    }
+
+    var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/;
+
+    if (!emailPattern.test(email)) {
+        emailError.innerHTML = "Invalid email format. Please enter a valid email address."
+        emailError.style.display = "block"
+        // alert("Invalid email format. Please enter a valid email address.");
+        return;
+    }  else {
+        emailError.innerHTML = ""
+        emailError.style.display = "none"
+        errorCount--
+    }
+
+
+    
+    if (password.trim() === "") {
+        passwordError.innerHTML = "Please enter your password."
+        passwordError.style.display = "block"
+        errorCount ++
+        // alert("Please enter your password.");
+        return;
+    }  else {
+        passwordError.innerHTML = ""
+        passwordError.style.display = "none"
+        errorCount--
+    }
+
+    if (password.length < 8) {
+        passwordError.innerHTML = "Password must be at least 8 characters long."
+        passwordError.style.display = "block"
+        errorCount ++
+        // alert("Password must be at least 8 characters long.");
+        return;
+    }   else {
+        passwordError.innerHTML = ""
+        passwordError.style.display = "none"
+        errorCount--
+    }
+
+    if(resigterErrorCount > 0){
+        alert("Form is invalid! fix the error.");
+    }
+    alert("Form is valid! You can submit it.");
+}
